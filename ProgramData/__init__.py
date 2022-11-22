1765e50ca566bd1f48d0e53d98088fb2d24ba9d3
Kita12123 <kthd12123@icloud.com>
2022-11-22 16:49:47 +0900

    setting.json -> system.json

--- ProgramData/__init__.py
+++ ProgramData/__init__.py
@@ -2,10 +2,10 @@ import os
 
 SYSTEMDIR = os.path.dirname(__file__)
 
-DATABASE = os.path.join(SYSTEMDIR, "database.db")
-TEMP_CSV = os.path.join(SYSTEMDIR, "download.csv")
-SYSTEM_JSON = os.path.join(SYSTEMDIR, "setting.json")
-USER_JSON = os.path.join(SYSTEMDIR, "user.json")
-DEBUG_LOG = os.path.join(SYSTEMDIR, "debug.txt")
-INFO_LOG = os.path.join(SYSTEMDIR, "info.txt")
-ERROR_LOG = os.path.join(SYSTEMDIR, "error.txt")
+DATABASE    = os.path.join(SYSTEMDIR, "database.db")
+TEMP_CSV    = os.path.join(SYSTEMDIR, "download.csv")
+SYSTEM_JSON = os.path.join(SYSTEMDIR, "system.json")
+USER_JSON   = os.path.join(SYSTEMDIR, "user.json")
+DEBUG_LOG   = os.path.join(SYSTEMDIR, "debug.txt")
+INFO_LOG    = os.path.join(SYSTEMDIR, "info.txt")
+ERROR_LOG   = os.path.join(SYSTEMDIR, "error.txt")
